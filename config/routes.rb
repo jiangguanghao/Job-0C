a65bc43051fe86658c9c088d69ed7c440a7193de
jiangguanghao <yeyingsuifeng@gmail.com>
2018-08-27 09:35:16 +0800

    install simpleform with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
   root 'welcome#index'
->>>>>>> 4d813e889b1f4b4722b7037e70961e842b37eeee
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
